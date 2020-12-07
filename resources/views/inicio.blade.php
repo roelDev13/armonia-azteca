5e05bf1ca3f58a9bfacf9a996f1c4e9b35704c9f
Roel <phoenixwings48@gmail.com>
2020-12-06 22:59:27 -0600

    Actualizacion

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -7,9 +7,9 @@
     <h1 style="font-size:50px">Armonia Azteca</h1><br>
     <div>
         <p>Stock!</p>  
-        <form class="form-inline">
-            <input type="email" class="form-control" size="50" placeholder="Encuentra lo que estas buscando">
-            <button type="button" class="btn btn-primary">Buscar</button>
+        <form class="form-inline" action="{{ URL::to('/search') }}" method="POST" role="search">
+            <input type="email" class="form-control" size="50" placeholder="Encuentra lo que estas buscando" name="q">
+            <button type="submit" class="btn btn-primary">Buscar</button>
         </form>
     </div>
 </div>
